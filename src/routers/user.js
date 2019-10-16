12984a2da40a2a8daed8457a4fad3c9dd00781bb
Abhishek Sharma <5h1dabhi@gmail.com>
2019-10-16 16:44:19 +0530

    Serving up files

--- src/routers/user.js
+++ src/routers/user.js
@@ -121,7 +121,21 @@ router.delete('/users/me/avatar', auth, upload.single('avatar'), async (req, res
     res.sendStatus(200)
 })
 
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
 
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+
+    } catch (error) {
+        res.status(404).send()
+    }
+})
 
 
 module.exports = router
